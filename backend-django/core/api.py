63597f5f847172af60c2c7ed7efad04026365902
Foshati <amirrezafoshati@gmail.com>
2024-09-07 12:46:58 +0330

    django ninja create first api

--- backend-django/core/api.py
+++ backend-django/core/api.py
@@ -1,12 +1,19 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
 
-app = NinjaAPI()
+api = NinjaAPI()
 
 
-@app.get("/fa")
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool
+    email: str = None
+
+
+@api.get("/fa")
 def hello(request):
     return "Hello, world!"
 
 
-
-
+@api.get("/me", response=UserSchema)
+def me(request):
+    return request.user
--- backend-django/core/urls.py
+++ backend-django/core/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from .api import app
+from .api import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", app.urls),
+    path("api/", api.urls),
     path("", include("api.urls")),
 ]
